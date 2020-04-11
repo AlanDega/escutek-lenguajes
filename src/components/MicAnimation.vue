5a7c0c95803f58c9dd77197dfb44316bbbc3e3f8
AlanDega <h-unit@protonmail.com>
2020-04-10 22:13:11 -0500

    c

---
+++ src/components/MicAnimation.vue
@@ -0,0 +1,13 @@
+<template>
+  <mic :loop="true"></mic>
+</template>
+
+<script>
+import mic from '@haiku/enjin1-mic/vue';
+
+export default {
+  components: {
+    mic
+  }
+}
+</script>
--- src/views/Game3.vue
+++ src/views/Game3.vue
@@ -1,25 +1,70 @@
 <template>
   <div>
-    <v-row justify="center">
-      <h1>Reto de pronunciación</h1>
+    <v-row justify="center" class="mt-12">
+      <h1 class="header">Reto de pronunciación</h1>
     </v-row>
-    <v-row justify="center">
-      <v-card height="240" width="500" class="challenge-card">
-        <v-row  class="fill-height" justify="center" align="center">
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col>
+        <v-divider color="indigo"></v-divider>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+    <v-row justify="center" class="challenge-card">
+      <v-card height="240" width="500" :class="compClasses">
+        <v-row class="fill-height" justify="center" align="center">
           <v-window vertical v-model="words">
-            <v-window-item v-for="(challenge, i) in challenges" :key="i">
-              <v-row >
-                <h1>Di: {{ challenge }}</h1>
+            <v-window-item
+              class="window-item"
+              v-for="(challenge, i) in challenges"
+              :key="i"
+            >
+              <v-row class="text-challenge" justify="center" align="center">
+                <h1>{{ challenge }}</h1>
               </v-row>
             </v-window-item>
           </v-window>
         </v-row>
       </v-card>
     </v-row>
+    <v-row justify="center">
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-row justify="center">
+          <div>
+            <MicAnimation @click="console.log('hey')" />
+          </div>
+        </v-row>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
 
     <v-row>
       <vue-speech class="speech" @onTranscriptionEnd="onEnd" />
     </v-row>
+    <v-footer color="black" absolute height="120">
+      <v-row justify="center">
+        <v-stepper v-model="stepa" class="stepper">
+          <v-stepper-header>
+            <v-stepper-step color="deep-purple accent-3" step="1" complete>{{
+              challenges[0]
+            }}</v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step color="deep-purple accent-3" step="2">{{
+              challenges[1]
+            }}</v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step color="deep-purple accent-3" step="3">{{
+              challenges[2]
+            }}</v-stepper-step>
+          </v-stepper-header>
+        </v-stepper>
+      </v-row>
+    </v-footer>
     <v-snackbar color="success" v-model="success_notif">
       <h1>Exito</h1>
     </v-snackbar>
@@ -27,22 +72,36 @@
 </template>
 
 <script>
-// let SpeechRecognition =
-//   window.SpeechRecognition || window.webkitSpeechRecognition;
-// let recognition = SpeechRecognition ? new SpeechRecognition() : false
+import MicAnimation from "../components/MicAnimation.vue";
+
 export default {
-  components: {},
+  components: {
+    MicAnimation
+  },
   data() {
     return {
+      stepa: 0,
       words: 0,
       word: "",
       text: "",
       sentences: null,
       challenges: ["moon", "winning", "breakfast"],
-      success_notif: false
+      success_notif: false,
+      hit: false,
+      miss: false,
+      base: true
     };
   },
   mounted() {},
+  computed: {
+    compClasses() {
+      return {
+        hit: this.hit,
+        miss: this.miss,
+        base: this.base
+      };
+    }
+  },
   watch: {
     data() {}
   },
@@ -51,10 +110,13 @@ export default {
       this.words = this.words + 1 === this.length ? 0 : this.words + 1;
     },
     onEnd({ lastSentence, transcription }) {
-      console.log("last", lastSentence);
+      
       this.word = lastSentence;
-      if (this.word === this.challenge) {
-        this.success_notif = true;
+      this.word.toLowerCase()
+      console.log("last", this.word);
+      if (this.word === this.challenges[0]) {
+        
+        this.stepa ++
       }
       console.log("transcription", transcription);
     }
@@ -63,16 +125,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.stepper {
+  width: 50%;
+}
+h1 {
+  font-size: 42px;
+}
 p {
-  font-size: 24px;
+  font-size: 1px;
   padding: 10px;
   color: black;
 }
 .speech {
   color: white;
 }
-.challenge-card {
+.base {
   background: white;
-  color:black
+  color: black;
+}
+
+.hit {
+  background: green;
+  color: white;
+}
+.miss {
+  background: red;
+  color: white;
+}
+.window-item {
+  width: 500px;
+  height: 100%;
+}
+.text-challenge {
+  height: 240px;
+}
+.challenge-card {
+  margin-top: 70px;
+}
+.header {
+  font-size: 48px;
 }
 </style>
